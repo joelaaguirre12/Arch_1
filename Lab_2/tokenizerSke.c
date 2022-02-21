b32d8623e6d85ccab899333908303e5404a8afa9
joelaaguirre12 <joelaaguirre12@gmail.com>
2022-02-21 14:07:20 -0700

    Added tokenizer file, made the file

---
+++ Lab_2/tokenizerSke.c
@@ -0,0 +1,171 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+/* Return true (non-zero) if c is the delimiter character
+   previously chosen by the user.
+   Zero terminators are not printable (therefore false) */
+bool delim_character(char c, char delim)
+{
+	if(c == '\0' || c != delim) {
+		/* If c is null-terminator or is not delim, return false */
+		return false;
+	}
+	else {
+		/* Otherwise c is not a null-terminator and is the delim */
+		return true;
+	}
+}
+
+/* Return true (non-zero) if c is a non-delimiter
+   character.
+   Zero terminators are not printable (therefore false) */
+bool non_delim_character(char c, char delim)
+{
+	if(c == '\0' || c == delim) {
+		/* If c is the null-terminator or is the delim */
+		return false;
+	}
+	else {
+		/* Otherwise c is not a null-terminator and is not the delim */
+		return true;
+	}
+}
+
+/* Returns a pointer to the first character of the next
+   word starting with a non-delimiter character. Return a zero pointer if
+   str does not contain any words.*/
+char *word_start(char *str, char delim)
+{
+	char *pchar;
+	pchar = str;
+	// if string is empty, return 0
+	if(strlen(str) == 0) {
+		return 0;
+	}
+	else {
+		// printf("Before increment: *pchar = '%c' pchar = %d\n", *pchar, pchar);
+		while (delim_character(*pchar, delim)) {
+			// printf("'%c' is not a delim, continuing loop...\n", *pchar);
+			pchar++;
+			// printf("Address: %d\n", pchar);
+			// printf("After increment: *pchar = '%c' pchar = %d\n", *pchar, pchar);
+		}
+		//printf("%s\n", pchar);
+		return pchar;
+	}
+}
+
+/* Returns a pointer to the first delimiter character of the zero
+   terminated string*/
+char *end_word(char *str,char delim)
+{
+	char *pchar;
+	pchar = str;
+	if(strlen(str) == 0) {
+		return 0;
+	}
+	else {
+		// printf("Before increment: *pchar = '%c' pchar = %d\n", *pchar, pchar);
+		while (non_delim_character(*pchar, delim)) {
+			// printf("'%c' is not a delim, continuing loop...\n", *pchar);
+			pchar++;
+			// printf("Address: %d\n", pchar);
+			// printf("After increment: *pchar = '%c' pchar = %d\n", *pchar, pchar);
+		}
+		return pchar;
+	}
+}
+
+/* Counts the number of words or tokens*/
+int count_tokens(char* str,char delim)
+{
+	int num_tok = 0;
+	char *pchar;
+	pchar = str;
+	if (strlen(str) == 0) {
+		return num_tok;
+	}
+	else {
+		while (*pchar != '\0') {
+			if (delim_character(*pchar, delim)) {
+				num_tok++;
+			}
+			pchar++;
+		}
+		num_tok++;
+		return num_tok;
+	}
+}
+
+/* Returns a freshly allocated new zero-terminated string
+   containing <len> chars from <inStr> */
+char *copy_str(char *inStr, short len, char delim)
+{
+	/* if user is asking for a string longer than the original string's length, return the length of the string */
+	if (len > strlen(inStr)) {
+		len = strlen(inStr);
+	}
+	char* string = (char*) malloc(len * sizeof(char));
+	char *pchar;
+	pchar = inStr;
+	int i;	
+	if (len == 0) {
+		return '\0';
+	}
+	else {
+		for (i = 0; i < len; i++) {
+			string[i] = *pchar;
+			//printf("String: %c *pchar: %c\n", string[i], *pchar);
+			pchar++;	
+		}
+		string[i] = '\0';
+		return string;
+	}
+}
+
+/* Returns a freshly allocated zero-terminated vector of freshly allocated
+   delimiter-separated tokens from zero-terminated str.
+   For example, tokenize("hello world string"), with a character delimiter
+   of a space " " would result in:
+     tokens[0] = "hello"
+     tokens[1] = "world"
+     tokens[2] = "string"
+     tokens[3] = 0
+*/
+char** tokenize(char* str, char delim)
+{
+	int length = count_tokens(str, delim);
+	int i;
+	char** tokens = (char**) malloc((strlen(str) + (length * 4)) * sizeof(char));
+	char *start = word_start(str, delim);
+	char *end = end_word(str, delim);
+	for (i = 0; i < length; i++) {
+		int wordLength = end-start;
+		//printf("Start: %s\n", start);
+		//printf("End: %s\n", end);
+		tokens[i] = copy_str(start, wordLength, delim);
+		//printf("Trying to find new word start\n");
+		start = word_start(end++, delim);
+		if(i == (length-1)){
+			end = str + ((strlen(str) * sizeof(char)));
+		} else { 
+			end = end_word(start, delim);
+		}
+		//printf("New Start: %s\n", start);
+		//printf("New End: %s\n", end);
+		printf("Token %d: %s\n", i, tokens[i]);
+	}
+	tokens[length] = '\0';
+	printf("Token %d: %s\n", i, tokens[i]);
+	return tokens;
+}
+
+void print_all_tokens(char** tokens, int size)
+{	
+	int i;
+	for (i = 0; i < size+1; i++) {
+		printf("Token %d: %s\n", i, tokens[i]);
+	}
+}
